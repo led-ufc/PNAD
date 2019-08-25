0999213e8320bed327f99105e58091be0d2e8625
Davi Ramalho <45436114+daviramalho@users.noreply.github.com>
2019-08-25 09:35:59 -0300

    Update SQL_PostgreSQL_Pandas-REV04.py

--- database_access/SQL_PostgreSQL_Pandas-REV04.py
+++ database_access/SQL_PostgreSQL_Pandas-REV04.py
@@ -6,19 +6,19 @@ import matplotlib.pyplot as plt
 
 #Connection data:
 database = input("Digite o nome do Banco de Dados a ser acessado:")
-postgreuser = input("Usuário:")
+postgresuser = input("Usuário:")
 postgrespassword = input("Senha:")
 hosts_ip = input("IP:")
-conn_port = input("Porta:")
+#conn_port = input("Porta:")
 
 
 # Connect the database
 conn = psycopg2.connect(
-    dbname = "database",
-    user = "postgresuser",
-    password = "postgrespassword",
-    host = "hosts_ip",
-    port = "conn_port"
+    dbname = database,
+    user = postgresuser,
+    password = postgrespassword,
+    host = hosts_ip,
+    port = 5432
 )
 
 # Open cursor to perform database operations
@@ -43,7 +43,7 @@ z = cur4.fetchall()
 
 d = {'id':x, 'cd_aponde':y, 'aa_km2':z}
 df = pd.DataFrame (data = d)
-print(df)
+df
 
 # Close comunication the database
 cur.close()
